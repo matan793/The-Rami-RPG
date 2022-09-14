f3c46fc4043aa0336ee2ea7b1d0968b0ce62f1e2
matan793 <mataneldar123@gmail.com>
2022-09-14 20:29:13 +0300

    added models and animations

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class Mover : MonoBehaviour
 {
     public Transform target;
-
+    
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +18,23 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButton(0))
         {
             MoveTOCursore();
             
         }
-       
-        
+
+        UpdateAnimator();
+
+
+    }
+    private void UpdateAnimator()
+    {
+        Vector3 velocity =  GetComponent<NavMeshAgent>().velocity;
+        Vector3 localvelocity =  transform.InverseTransformDirection(velocity);
+        float speed = localvelocity.z;
+        GetComponent<Animator>().SetFloat("FSpeed", speed);
+
     }
     private void MoveTOCursore()
     {
@@ -33,10 +43,10 @@ public class Mover : MonoBehaviour
         bool hit = Physics.Raycast(ray, out raycastHit);
         if (hit)
         {
+         
             Debug.Log(hit);
             GetComponent<NavMeshAgent>().destination = raycastHit.point;
-
         }
-        
+       
     }
 }
